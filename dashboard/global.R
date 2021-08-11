8b96bfea84051f95a16838ce2e3d53e923f3db06
Melanie J <melaniej.1@outlook.com>
2021-08-11 12:20:06 +0100

    app draft 1

--- dashboard/global.R
+++ dashboard/global.R
@@ -3,13 +3,30 @@ library(tidyverse)
 library(shiny)
 library(DT)
 library(shinydashboard)
+library(leaflet)
+library(sf)
+library(here)
+library(plotly)
+life_expectancy_data <- read_csv("~/public_health_dashboard/clean_data/life_expectancy_clean.csv")
+life_expectancy_data_2 <- life_expectancy_data %>%
+  filter(age == "0 years" & date_code == "2017-2019")
 
 
+life_expectancy_data_2[life_expectancy_data_2=="Na h-Eileanan Siar"]<- "Eilean Siar"
 
+life_expectancy_round <- round(life_expectancy_data_2$value, 2)
+life_expectancy_data_2 <- life_expectancy_data_2 %>%
+  mutate(value = life_expectancy_round)
+
+colnames(life_expectancy_data)[which(names(life_expectancy_data) == "sex")] <- "gender"
+colnames(life_expectancy_data_2)[which(names(life_expectancy_data_2) == "sex")] <-"gender"
+
+scotland_shape <- st_read(here("~/public_health_dashboard/clean_data/shape_data/pub_las.shp")) %>% 
+ st_transform("+proj=longlat +datum=WGS84")
 
 
 
 
 
 #Launch App
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
--- dashboard/server.R
+++ dashboard/server.R
@@ -1,48 +1,62 @@
 # server ------------------------------------------------------------------
 
-server <- function(input, output, session) {
+server <- function(input, output) {
   
   # OVERVIEW TAB ---------------------------------------------------------------
   
-overview_data <- reactive({
-    [WRANGLE DATA FOR LIFE EXPECTANCY]
+life_exp_filtered <- reactive({
+  
+  life_expectancy_data_2 %>% 
+    filter(gender == input$gender_input) %>% 
+    left_join(scotland_shape, by = c("area" = "local_auth")) %>%
+    st_as_sf() 
+})
+
+   
+      output$life_exp_map <- renderLeaflet({
+        
+        life_exp_filtered <- life_exp_filtered()
+        
+        bins <- c(70, 72, 74, 76, 78, 80, 82, 84, 86)
+        pal <- colorBin("Blues", domain = test$value, bins = bins)
+        life_exp_labels <- sprintf(
+          "<strong>%s</strong><br/>%g years",
+          life_expectancy_data_2$local_authority, life_expectancy_data_2$value
+        ) %>% 
+          lapply(htmltools::HTML)
+        
+        life_exp_filtered %>%
+          leaflet() %>% 
+          setView(lng = -4.2026, lat = 57.8, zoom = 5.5, options = list()) %>%
+          addTiles() %>% 
+          addPolygons(fillColor = ~pal(value),
+                      weight = 0.5,
+                      opacity = 0.5,
+                      color = "black",
+                      fillOpacity = 0.5,
+                      highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                          bringToFront = TRUE),
+                      label = life_exp_labels,
+                      labelOptions = labelOptions(
+                        style = list("font-weight" = "normal", padding = "3px 8px"),
+                        textsize = "15px",
+                        direction = "auto")) %>%
+          addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
+                    position = "bottomright")
+      })
+      
+
+
+
     
-    output$life_expectancy_map({
-      [LEAFLET MAP CODE]
-    }),
-    
-    output$life_expectancy_plot({
-      [GGPLOT FOR LIFE EXPECTANCY]
+    output$life_expectancy_plot <- renderPlotly({
+        ggplot(life_expectancy_data_2, aes(x=local_authority, y=value, fill=sex)) + 
+          geom_bar(stat="identity", color="black", width=0.7, position=position_dodge(width=0.9)) +
+          geom_errorbar(aes(ymin=value, ymax=value), width=.2,
+                        position=position_dodge(.9)) +
+          theme(axis.text.x = element_text(angle = 90))+
+          labs(title="Life Expectancy In Scotland By Local Authority Area From 2009 to 2019", 
+               x="Local Authority Area", y = "Life Expectancy In Years")
     })
-  })
-  
-  # DRUGS TAB -------------------------------------------------------------
-  
-  
-drugs_data <- reactive({
-    [WRANGLE DATA FOR DRUGS]
-    
-    output$drug_map({
-      [LEAFLET MAP CODE]
-    }),
-    
-    output$drug_plot({
-      [GGPLOT FOR DRUGS]
-    })
-  })
-  
-  
-  # ALCOHOL TAB -------------------------------------------------------------
-  
-alcohol_data <- reactive({
-    [WRANGLE DATA FOR ALCOHOL]
-    
-    output$alcohol_map({
-      [LEAFLET MAP CODE]
-    }),
-    
-    output$alcohol_plot({
-      [GGPLOT FOR ALCOHOL]
-    })
-    
-  })
+
+}
