2a35f02dd0fd2c1154f981e9c4e0db1e2204c5d8
tomdavie <85116961+tomdavie@users.noreply.github.com>
2021-08-11 16:12:06 +0100

    Overview page

--- dashboard/server.R
+++ dashboard/server.R
@@ -1,266 +1,140 @@
 # server ------------------------------------------------------------------
 
-server <- function(input, output, session) {
-
-# DRUGS TAB -------------------------------------------------------------
-
-# DRUG MAP SELECTINPUT CHANGES
-observe({
-  # Update drug map selections based on year input
-  if (input$drug_map_year != "All") {
-    drug_choice_selection <- drug_deaths %>%
-      filter(year == input$drug_map_year) %>%
-      select(drug_name) %>%
-      arrange(drug_name) %>%
-      pull(drug_name)
-
-    updateSelectInput(session,
-                      "drug_map_name",
-                      label = "Select Drug:",
-                      choices = drug_choice_selection,
-                      selected = "All drug-related deaths")
-  } else {
-    drug_choice_selection <- sort(unique(drug_deaths$drug_name))
-
-    updateSelectInput(session,
-                      "drug_map_name",
-                      label = "Select Drug:",
-                      choices = drug_choice_selection,
-                      selected = "All drug-related deaths")
-  }
-})
-
-# DRUG MAP DATA
-drugs_map_data <- reactive({
-
-  # If all years is selected, filter for all years
-  if(input$drug_map_year == "All") {
-    drugs_map_year_selection <- sort(unique(drug_deaths$year))
-  } else {
-    drugs_map_year_selection <- input$drug_map_year
-  }
-
-  # Filter data for leaflet plot
-  drugs_map_data <- drug_deaths %>%
-    filter(council_area != "Scotland",
-           drug_name %in% input$drug_map_name,
-           year %in% drugs_map_year_selection) %>%
-    group_by(council_area) %>%
-    summarise(drug_name = drug_name, num_deaths = sum(num_deaths)) %>%
-    # Join with shape data
-    left_join(scotland_shape, by = c("council_area" = "local_auth")) %>%
-    # Convert to shape data from data frame
-    st_as_sf()
-})
-
-
-# DRUG MAP OUTPUT
-output$drug_map <- renderLeaflet({
-
-  drugs_map_data <- drugs_map_data()
-
-  # Labels variable for leaflet plot
-  drugs_map_labels <- sprintf(
-    "<strong>%s</strong><br/>%g deaths",
-    drugs_map_data$council_area,
-    drugs_map_data$num_deaths) %>%
-    lapply(htmltools::HTML)
-
-  if (input$drug_map_year == "All" & input$drug_map_name == "All drug-related deaths"){
-    drugs_map_bins <- c(0, 50, 100, 200, 500, 1000, 2000, Inf)
-  } else {
-    drugs_map_bins <- c(0, 5, 15, 30, 50, 100, 250, Inf)
-  }
-
-  pal <- colorBin("Purples", domain = drugs_map_data$num_deaths, bins = drugs_map_bins)
-
-  # Initialise plot
-  drugs_map_data %>%
-    leaflet() %>%
-    setView(lng = -4.2026, lat = 57.8, zoom = 6, options = list()) %>%
-    addProviderTiles(providers$CartoDB.Positron) %>%
-    addPolygons(fillColor = ~pal(num_deaths),
-                weight = 0.75,
-                opacity = 1,
-                color = "black",
-                dashArray = "2",
-                fillOpacity = 0.9,
-                highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                    bringToFront = TRUE),
-                label = drugs_map_labels,
-                labelOptions = labelOptions(
-                  style = list("font-weight" = "normal", padding = "3px 8px"),
-                  textsize = "15px",
-                  direction = "auto")) %>%
-    addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
-              position = "bottomright")
-
-  # DRUG PLOT DATA
-  drug_plot_data <- reactive({
-    drug_plot_data <- drug_deaths %>%
-      filter(drug_name %in% input$drug_plot_name,
-             council_area %in% input$drug_plot_area)
+server <- function(input, output) {
+  
+  output$value <- renderText({ input$about })
+  
+  
+  output$avg_m <- renderValueBox({
+    
+    valueBox(
+      value = 
+        tags$p(     ,
+               style = "font-size: 80%;"),
+      subtitle = "Most Popular Game",
+      icon = icon("heart"),
+      color = "blue"
+    )
   })
-
-  # DRUG PLOT OUTPUT
-  output$drug_plot <- renderPlotly({
-
-    min_year <- drug_plot_data() %>%
-      slice_min(year) %>%
-      pull(year)
-
-    max_year <- drug_plot_data() %>%
-      slice_max(year) %>%
-      pull(year)
-
-
-    ggplotly(
-      drug_plot_data() %>%
-        ggplot() +
-        aes(x = year, y = num_deaths) +
-        geom_line(colour = "#605ca8", alpha = 0.75, size = 1.5) +
-        geom_point(aes(text=sprintf("Year: %g<br>Deaths: %g", year, num_deaths)),
-                   colour = "black", size = 2, alpha = 0.9) +
-        scale_x_continuous(breaks = c(min_year:max_year)) +
-        scale_y_continuous(n.breaks = 8) +
-        theme_minimal() +
-        theme(panel.grid.major = element_line(colour = "grey"),
-              plot.background = element_rect(fill = "#ecf0f6"),
-              panel.background = element_rect(fill = "#ecf0f6")),
-        tooltip = c("text")
-    ) %>%
-      config(displayModeBar = FALSE) %>%
-      layout(xaxis = list(title = "Year"),
-             yaxis = list(title = "Number of Deaths"),
-             title = list(text = paste0(
-                                        'Number of deaths in ', input$drug_plot_area, ' from ', min_year, '-', max_year,
-                                        '<br>',
-                                        '<sup>',
-                                        input$drug_plot_name,
-                                        '</sup>',
-                                        '<br>')),
-             margin = list(t = 50, b = 50, l = 50) # to fully display the x and y axis labels
-      )
+  
+  output$avg_f <- renderValueBox({
+    
+    valueBox(
+      value = 
+        tags$p(    ,
+               style = "font-size: 80%;"),
+      subtitle = "Most Popular Game",
+      icon = icon("heart"),
+      color = "blue"
+    )
   })
+  
+  output$life_exp_area <- renderValueBox({
+    
+    valueBox(
+      value = 
+        tags$p(    ,
+               style = "font-size: 80%;"),
+      subtitle = "Most Popular Game",
+      icon = icon("heart"),
+      color = "blue"
+    )
+  })
+  
+  output$most_drug_death <- renderValueBox({
+    
+    valueBox(
+      value = 
+        tags$p(drug_deaths %>% 
+                 filter(drug_name != "All drug-related deaths" & year == 2019) %>% 
+                 slice_max(num_deaths) %>% 
+                 pull(drug_name),
+               style = "font-size: 80%;"),
+      subtitle = "Most Lethal Drug 2019",
+      icon = icon("pills"),
+      color = "purple"
+    )
+  })
+  
+  output$total_drug_death <- renderValueBox({
+    
+    valueBox(
+      value = 
+        tags$p(drug_deaths %>% 
+                 filter(council_area == "Scotland" &
+                          drug_name == "All drug-related deaths" &
+                          year == 2019) %>%
+                 pull(num_deaths),
+               style = "font-size: 80%;"),
+      subtitle = "Drug Deaths 2019",
+      icon = icon("pills"),
+      color = "purple"
+    )
+  })
+  
+  output$worst_drug_area <- renderValueBox({
+    
+    valueBox(
+      value = 
+        tags$p(drug_deaths %>% 
+                 filter(drug_name == "All drug-related deaths" &
+                          year == 2019 &
+                          council_area != "Scotland") %>%
+                 slice_max(1) %>% 
+                 pull(council_area),
+               style = "font-size: 80%;"),
+      subtitle = "Council Area with Most Drug Deaths 2019",
+      icon = icon("home"),
+      color = "purple"
+    )
+  })
+  
+  output$alcohol_age <- renderValueBox({
+    
+    valueBox(
+      value = 
+        tags$p(alcohol_deaths %>% 
+                 drop_na(age_group) %>% 
+                 filter(year_of_death == 2019) %>% 
+                 slice_max(count, n = 1, with_ties = FALSE) %>% 
+                 pull(age_group),
+               style = "font-size: 80%;"),
+      subtitle = "Age of Most Alcohol-related Mortality in 2019",
+      icon = icon("wine-glass"),
+      color = "green"
+    )
+  })
+  
+  output$total_alc_death <- renderValueBox({
+    
+    valueBox(
+      value = 
+        tags$p(alcohol_deaths %>% 
+                 drop_na(age_group) %>% 
+                 filter(year_of_death == 2019) %>% 
+                 summarise(count = sum(count)) %>% 
+                 pull(count),
+               style = "font-size: 80%;"),
+      subtitle = "Total Alcohol Deaths 2019",
+      icon = icon("wine-glass"),
+      color = "green"
+    )
+  })
+  
+  output$worst_alcohol_area <- renderValueBox({
 
-})
-
-# ALCOHOL TAB -------------------------------------------------------------
-
-alcohol_deaths_filtered <- reactive({
-
-if(input$gender_input == "All") {
- alcohol_plot_gender_selection <- sort(unique(alcohol_deaths$gender))
-} else {
- alcohol_plot_gender_selection <- input$gender_input
-}
-
-
-if(input$age_input == "All") {
- alcohol_plot_age_selection <- sort(unique(alcohol_deaths$age_group))
-} else {
- alcohol_plot_age_selection <- input$age_input
-}
-
-
-alcohol_deaths %>%
- filter(age_group != "all_ages" & age_group != "average_age") %>%
- #mutate(gender = factor(gender, levels = c("Male", "Female"))) %>%
- filter(gender %in% alcohol_plot_gender_selection,
-        age_group %in% alcohol_plot_age_selection) %>%
- group_by(gender, year_of_death) %>%
- summarise(count = sum(count))
-
-})
-
-#ALCOHOL MAP OUTPUT
-
-alcohol_area_filtered <- reactive({
-
-  if(input$year_input == "All") {
-    alcohol_map_year_selection <- sort(unique(alcohol_area$year_of_death))
-  } else {
-    alcohol_map_year_selection <- input$year_input
-  }
-
-  alcohol_area %>%
-    select(-1) %>%
-    filter(area != "All Scotland") %>%
-    group_by(area) %>%
-    filter(year_of_death %in% alcohol_map_year_selection) %>%
-    summarise(area, count) %>%
-    left_join(scotland_shape, by = c("area" = "local_auth")) %>%
-    st_as_sf()
-
-})
-
-    output$alcohol_map <- renderLeaflet({
-
-      alcohol_area_filtered <- alcohol_area_filtered()
-
-      bins <- c(0, 25, 50, 100, 150, 200)
-      pal <- colorBin("Greens", domain = alcohol_area_filtered$count, bins = bins)
-
-      alcohol_map_labels <- sprintf(
-        "<strong>%s</strong><br/>%g deaths",
-        alcohol_area_filtered$area, alcohol_area_filtered$count
-      ) %>%
-        lapply(htmltools::HTML)
-
-      alcohol_area_filtered %>%
-        leaflet() %>%
-        setView(lng = -4.2026, lat = 57.8, zoom = 6, options = list()) %>%
-        addProviderTiles(providers$CartoDB.Positron) %>%
-        addPolygons(fillColor = ~pal(count),
-                    weight = 0.5,
-                    opacity = 0.5,
-                    color = "black",
-                    fillOpacity = 0.5,
-                    highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                        bringToFront = TRUE),
-                    label = alcohol_map_labels,
-                    labelOptions = labelOptions(
-                      style = list("font-weight" = "normal", padding = "3px 8px"),
-                      textsize = "15px",
-                      direction = "auto")) %>%
-        addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
-                  position = "bottomright")
-    })
-
-#ALCOHOL PLOT OUTPUT
-
-    output$alcohol_plot <- renderPlotly({
-      ggplotly(
-      alcohol_deaths_filtered() %>%
-        ggplot() +
-        aes(x = year_of_death, y = count, fill = gender) +
-        geom_col(aes(text=sprintf("Year: %g<br>Deaths: %g<br>Gender: %s", year_of_death, count, gender))) +
-        scale_x_continuous(breaks = c(2009:2019)) +
-        scale_fill_manual(values = c("Female" = "#bae3b5",
-                                     "Male" = "#73c375")) +
-       # theme(legend.position = "none") +
-        theme_minimal()+
-        theme(panel.grid.major = element_line(colour = "grey"),
-              plot.background = element_rect(fill = "#ecf0f6"),
-              panel.background = element_rect(fill = "#ecf0f6")),
-       tooltip = c("text")
-      ) %>%
-        config(displayModeBar = FALSE) %>%
-        layout(legend = list(orientation = 'h',
-                             yanchor="bottom",
-                             y=0.99,
-                             xanchor="right",
-                             x=1),
-               xaxis = list(title = "Year"),
-               yaxis = list(title = "Number of Deaths"),
-               title = list(text = paste0(
-                 'Number of Alcohol Deaths per Year',
-                 '<br>',
-                 '<sup>',
-                 'Gender: ', input$gender_input, '  -  Age Group: ', input$age_input,
-                 '</sup>',
-                 '<br>')),
-               margin = list(t = 50, b = 50, l = 50) # to fully display the x and y axis labels
-        )
-    })
+    valueBox(
+      value = 
+        tags$p(alcohol_area %>% 
+                 filter(area != "All Scotland" &
+                          year_of_death == 2009) %>% 
+                 slice_max(count, n = 1) %>% 
+                 pull(count),
+               style = "font-size: 80%;"),
+      subtitle = "Area with Most Alcohol Deaths 2019",
+      icon = icon("home"),
+      color = "green"
+    )
+  })
+  
 }
--- dashboard/ui.R
+++ dashboard/ui.R
@@ -1,125 +1,52 @@
-
-ui <- dashboardPage(skin = "black",
-
+  
+  ui <- dashboardPage(skin = "black",
+  
                     # ShinyDashboard tabs
                     dashboardHeader(title = "Health in Scotland - The Drug Crisis"),
                     dashboardSidebar(
                       sidebarMenu(
                         menuItem("Scotland Health Overview", tabName = "overview"),
+                        menuItem("Life Expectancy", tabName = "life_expectancy"),
                         menuItem("Drug Deaths", tabName = "drug_deaths"),
                         menuItem("Alcohol Deaths", tabName = "alcohol_deaths")
                       )
                     ),
-
+  
                     dashboardBody(
                       tabItems(
                         # OVERVIEW PANEL ---------------------------------------
-
-                        tabItem(tabName = "overview",
-
-                        ),
-
-
-                        # DRUGS PANEL -------------------------------------------------------------
-                        tabItem(tabName = "drug_deaths",
-                                fluidRow(
-                                  column(width = 6,
-                                         box(width = NULL, solidHeader = TRUE, background = "purple",
-                                             column(width = 6, align = "center",
-                                                    selectInput("drug_map_year",
-                                                                label = "Year:",
-                                                                choices = c("All",
-                                                                            sort(unique(drug_deaths$year))),
-                                                                selected = "All")
-                                             ),
-                                             column(width = 6, align = "center",
-                                                    selectInput("drug_map_name",
-                                                                label = "Select Drug:",
-                                                                choices = sort(unique(drug_deaths$drug_name)),
-                                                                selected = "All drug-related deaths")
-                                             )
-
-                                         ),
-
-
-
-
-                                         leafletOutput("drug_map", height = 600)
-
-
-                                  ),
-
-                                  column(width = 6,
-
-                                         box(width = NULL, solidHeader = TRUE, background = "purple",
-                                             column(width = 6, align = "center",
-                                                    selectInput("drug_plot_area",
-                                                                label = "Select Area:",
-                                                                choices = c(unique(drug_deaths$council_area)))
-                                             ),
-                                             column(width = 6, align = "center",
-                                                    selectInput("drug_plot_name",
-                                                                label = "Select Drug:",
-                                                                choices = sort(unique(drug_deaths$drug_name)),
-                                                                selected = "All drug-related deaths")
-                                             )
-
-                                         ),
-
-                                         plotlyOutput("drug_plot", height = 600, reportTheme = TRUE)
-
-                                  )
-
-
-                                )),
-
-
-                                tabItem(tabName = "alcohol_deaths",
-                                fluidRow(
-                                    column(width = 6,
-                                         box(width = NULL, solidHeader = TRUE, background = "green",
-                                             column(width = 12, align = "center",
-                                                    selectInput("year_input",
-                                                         label = "Year:",
-                                                         choices = c("All",
-                                                                     sort(unique(alcohol_area$year_of_death))),
-                                                         selected = "All",
-                                                         width = "50%"))),
-
-
-
-                                                leafletOutput("alcohol_map", height = 550)
-
-
-                                  ),
-
-
-
-
-                                  column(width = 6, align = "center",
-                                         fluidRow(
-                                         box(width = 12, solidHeader = TRUE, background = "green",
-                                             column(width = 6,
-                                                    selectInput("gender_input",
-                                                                label = "Gender:",
-                                                                choices = c("All",
-                                                                            sort(unique(alcohol_deaths$gender))),
-                                                                selected = "All")),
-                                             column(width = 6,
-                                                    selectInput("age_input",
-                                                                label = "Age Group:",
-                                                                choices = c("All",
-                                                                            unique(alcohol_deaths$age_group)),
-                                                                selected = "All")))),
-                                         fluidRow(
-                                         plotlyOutput("alcohol_plot", width = "95%", height = 550)
-                                         )
-
-
-                                  )
-
+  
+                       
+                                tabItem("overview",
+                                        fluidPage(
+                                        fluidRow(
+                                          h1("Scotland Health Overview "),
+                                          p("Our country has an escalating problem with drugs deaths. 
+                                            In this dashboard we take a look at recent trends in drug
+                                            and alcohol deaths across the country.",
+                                            style = "font-family: 'arial'; font-si16pt")                                ),
+                                        
+                                        fluidRow(
+                                          valueBoxOutput("avg_m"),
+                                          valueBoxOutput("avg_f"),
+                                          valueBoxOutput("life_exp_area")
+                                        ),
+                                        fluidRow(
+                                          valueBoxOutput("most_drug_death"),
+                                          valueBoxOutput("total_drug_death"),
+                                          valueBoxOutput("worst_drug_area")
+                                        ),
+                                          
+                                        fluidRow(
+                                          valueBoxOutput("alcohol_age"),
+                                          valueBoxOutput("total_alc_death"),
+                                          valueBoxOutput("worst_alcohol_area")
+                                        ))
+                                          
+                                        )
                                 )
                         )
-                      )
                     )
-)
+  
+  
+                       
