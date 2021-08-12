5e783226903953731bf02480e0358eb85ebf8330
undecipherable <82640195+undecipherable@users.noreply.github.com>
2021-08-12 08:43:38 +0100

    Update simd_cleaning_script.R

--- data_cleaning_scripts/simd_cleaning_script.R
+++ data_cleaning_scripts/simd_cleaning_script.R
@@ -1,10 +1,12 @@
-llibrary(tidyverse)
+library(tidyverse)
 library(janitor)
 library(readxl)
 library(here)
 
+# Read in SIMD data
 simd_2020 <- read_excel(here("raw_data/simd_2020.xlsx"), sheet = 3)
 
+# Select relevant columns, and rename
 simd_trimmed <- simd_2020 %>%
   select(DZ, SIMD2020v2_Vigintile, SIMD2020v2_Decile, SIMD2020v2_Quintile, LAcode, LAname) %>%
   rename(data_zone = DZ,
@@ -14,6 +16,7 @@ simd_trimmed <- simd_2020 %>%
          la_code = LAcode,
          la_name = LAname)
 
+# Covert to long format, summarise counts of each band and rank in each council area.
 simd_long <- simd_trimmed %>%
   pivot_longer(vigintile:quintile, names_to = "band", values_to = "rank") %>%
   group_by(la_name, la_code, band, rank) %>%
